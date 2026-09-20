9942bef58cea9a04c8e0a8462ca53559e8013f39
Ahmad Sayfudin <mahallaris@gmail.com>
2026-09-20 23:14:14 +0700

    Add relational operators demonstration in main.cpp

---
+++ 06-operator-perbandingan-(Relational-Operators)/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int nilaiA = 10;
+    int nilaiB = 5;
+
+    std::cout << "Apakah 10 > 5? " << (nilaiA > nilaiB) << "\n";
+    std::cout << "Apakah 10 == 5? " << (nilaiA == nilaiB) << "\n";
+    std::cout << "Apakah 10 != 5? " << (nilaiA != nilaiB) << "\n";
+
+    return 0;
+}
