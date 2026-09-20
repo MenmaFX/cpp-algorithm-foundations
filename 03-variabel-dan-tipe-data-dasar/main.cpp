e4c2c2338b451ea214b446d6a4f76d50b5cfa988
Ahmad Sayfudin <mahallaris@gmail.com>
2026-09-20 17:28:16 +0700

    Add main.cpp with basic variable declarations

---
+++ 03-variabel-dan-tipe-data-dasar/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string nama = "Ahmad Sayfudin"; // Sudah diperbaiki jadi std::
+    int umur = 16;
+    double ipk = 3.85;
+
+    std::cout << "Nama: " << nama << "\n";
+    std::cout << "Umur: " << umur << " tahun\n";
+    std::cout << "IPK: " << ipk << "\n";
+
+    return 0;
+}
