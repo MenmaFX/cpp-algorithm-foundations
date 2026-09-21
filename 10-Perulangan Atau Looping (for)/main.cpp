10028a0c952a918f465ebe4dda7db825106210e8
Ahmad Sayfudin <mahallaris@gmail.com>
2026-09-21 21:26:20 +0700

    Add loop to print numbers from 1 to 5

---
+++ 10-Perulangan Atau Looping (for)/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    //mencatak angka 1 sampai 5 menggunakan perulangan for
+    for (int i = 1; i <=5 ; i++) {
+        std::cout << "perulangan ke-" << i << "\n";
+    }
+
+    return 0;
+}
