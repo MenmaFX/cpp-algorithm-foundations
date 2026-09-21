292bb053535f6a2be823b4b939258ee19a8c4d83
Ahmad Sayfudin <mahallaris@gmail.com>
2026-09-21 22:19:32 +0700

    Add countdown loop in main.cpp
    Implement countdown loop using while statement.

---
+++ 11-Perulangan Atau Looping (while)/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int i = 5;
+
+    // Perulangan hitung mundur selama nilai 1 lebih besar dari 0
+    while (i > 0) {
+        std::cout << "hitung mundur: " << i << "\n";
+        i--; // Mengurangi nilai i sebanyak 1 (decrement)
+    }
+
+    std::cout << "roket meluncur nih: \n";
+
+    return 0;
+}
